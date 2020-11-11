e5b466ded372bbeede1483901bc8a41ccf973d41
zoltanmarai <zoltanmarai51@gmail.com>
2020-11-11 17:34:58 +0100

    Mondatjelek kiküszöbölése.

--- src/Szemantika1.java
+++ src/Szemantika1.java
@@ -3,36 +3,33 @@ import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
-import java.util.TreeMap;
 
 public class Szemantika1 {
 
-    static Map<String, Integer> szavak = new TreeMap<>();
-    // public static void main(String[] args) throws FileNotFoundException {
-    //    readFile();
+        static Map<String,Integer> szavak = new HashMap<>();
+       // public static void main(String[] args) throws FileNotFoundException {
+        //    readFile();
 
-    //  }
+      //  }
 
-    public static void readFile(String path) throws FileNotFoundException {
-        String[] s;
+        public static void readFile(String path) throws FileNotFoundException {
+            String[] s;
 
-        Scanner sc = new Scanner(new File(path));
-
-        while (sc.hasNextLine()) {
-            String line = sc.nextLine();
-            s = line.split(" ");
-            for (int i = 0; i < s.length; i++) {
-                if (szavak.containsKey(s[i])) {
-                    szavak.put(s[i], szavak.get(s[i]) + 1);
-                } else {
-                    szavak.put(s[i], 1);
+            Scanner sc = new Scanner(new File(path)) ;
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                s = line.split(" ");
+                for (int i = 0; i < s.length; i++) {
+                    if(szavak.containsKey(s[i])){
+                        szavak.put(s[i],szavak.get(s[i])+1);
+                    } else {
+                        szavak.put(s[i],1);
+                    }
                 }
+
             }
-
+            System.out.println(szavak);
         }
-
-        System.out.println(szavak);
     }
-}
 
 
