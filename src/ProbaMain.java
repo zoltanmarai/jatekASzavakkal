52469b9e2f450d5959fe46115aa2e6bdcde753f7
peteri gabor <peteri@t-online.hu>
2020-11-11 16:13:00 +0100

    menu2

--- src/ProbaMain.java
+++ src/ProbaMain.java
@@ -1,3 +1,65 @@
-public class ProbaMain {
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+
+    public class ProbaMain {
+
+
+
+        public static void main(String[] args) throws FileNotFoundException {
+            logIn();
+        }
+
+
+        public static void logIn() throws FileNotFoundException{
+            Scanner sc = new Scanner(System.in);
+            int menuSzam;
+            do {
+
+                System.out.println("   <<<< Udvozoljuk a SZOJATEK programban >>>>");
+                System.out.println();
+                System.out.println(" legyen kedves valasztani az alabbi menupontok kozul: ");
+                System.out.println();
+                System.out.println("Szemantikai elemzes - beepitett referencia szoveggel:  ---> 1");
+                System.out.println();
+                System.out.println("Szemantikai elemzes - sajat referencia szoveggel:  ---> 2");
+
+                System.out.println("bullshit generator   ---> 3");
+                System.out.println();
+                System.out.println("bullshit generator - sajat referencia szoveggel:  ---> 4");
+
+                System.out.println();
+                System.out.println(" kilepes:  ---> 0");
+
+                menuSzam = sc.nextInt();
+
+                switch (menuSzam) {
+                    case 1:
+                       Szemantika1.readFile();
+                        continue;
+
+                    case 2:
+
+                        //valami
+                        break;
+
+                    case 3:
+
+                        //to write
+                        break;
+
+                    case 4:
+
+                        //akarmi
+                        break;
+
+                    default:
+
+                        //default eset
+                }
+
+            } while (menuSzam == 0);
+            System.out.println("goooodbye");
+
+    }
 }
